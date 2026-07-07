a9ce8e44500ca5017b22e4a3545ff12187cb15a6
aarushhere128-hub <aarushhere128@gmail.com>
2026-07-07 18:02:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function generateRoadmap(){
+
+    let goal = document.getElementById("goalInput").value;
+
+    document.getElementById("result").innerHTML =
+    "Creating your roadmap for: " + goal;
+
+}
