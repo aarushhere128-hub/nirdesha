2ee2da30881efdd32eaa532a380e101b72677578
aarushhere128-hub <aarushhere128@gmail.com>
2026-07-08 19:25:37 +0530

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,37 @@
+import { auth } from "./firebase.js";
+import { onAuthStateChanged, signOut } 
+from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+
+const welcome = document.getElementById("welcome");
+const logoutBtn = document.getElementById("logoutBtn");
+
+
+onAuthStateChanged(auth, (user) => {
+
+
+    if (user) {
+
+        welcome.textContent =
+        "Welcome, " + user.displayName;
+
+
+    } 
+    
+    else {
+
+        window.location.href = "auth.html";
+
+    }
+
+});
+
+
+
+logoutBtn.addEventListener("click", async () => {
+
+    await signOut(auth);
+
+    window.location.href = "auth.html";
+
+});
