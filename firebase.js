92e8d8e4b492e2a7d8478374d039ddbab02e12e9
aarushhere128-hub <aarushhere128@gmail.com>
2026-07-08 15:22:12 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,11 @@
 
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
-  // TODO: Add SDKs for Firebase products that you want to use
+import {
+  getFirestore,
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";  // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
@@ -18,4 +21,4 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebas
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-export { db };
+export { db, collection, addDoc };
